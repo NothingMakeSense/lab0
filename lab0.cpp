1a86497c7f028b8a90e2900bad4734378f346b10
NothingMakeSense <ruslan.meteor@gmail.com>
2022-09-08 22:51:00 +0300

    Добавьте файлы проекта.

---
+++ lab0.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+const char* FIO = "Катюхин Руслан Станиславович";
+const char* GROUP = "ВИ921Б";
+
+int main()
+{
+    // Включение поддержки кириллицы 
+    setlocale(LC_ALL, "Russian");   
+
+    printf("%s, %s \n", FIO, GROUP);
+
+    return 0;
+}
+
