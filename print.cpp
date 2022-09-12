0c9094f3c3fbe7db9306551ef15f374121b58600
NothingMakeSense <ruslan.meteor@gmail.com>
2022-09-12 22:43:50 +0300

    Task 2 update

---
+++ print.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "print.h"
+
+// Метод для вывода данных в консоль
+void print(char* FIO, char* GROUP)
+{
+	printf("%s, %s \n", FIO, GROUP);
+}
---
+++ print.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Метод для вывода данных в консоль
+void print(char*, char*);
